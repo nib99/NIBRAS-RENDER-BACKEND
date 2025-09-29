e6e786a8684aa0b62669bfa80ab8be44a2e73663
Nibras Ahmed <odaabultum116@gmail.com>
2025-09-29 14:28:43 +0300

    Update users.js
    git add .
    git commit -m "Add missing users models"
    git push origin main

--- models/users.js
+++ models/users.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Name is required'],
     trim: true,
-    maxlength: [50, 'Name cannot be more than 50 characters']
+    maxlength: [50, 'Name cannot exceed 50 characters']
   },
   email: {
     type: String,
     required: [true, 'Email is required'],
     unique: true,
     lowercase: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please provide a valid email'
-    ]
+    validate: [validator.isEmail, 'Please provide a valid email']
   },
   password: {
     type: String,
@@ -28,78 +27,57 @@ const userSchema = new mongoose.Schema({
     enum: ['user', 'admin'],
     default: 'user'
   },
-  avatar: {
-    type: String,
-    default: ''
-  },
-  phone: {
-    type: String,
-    default: ''
-  },
-  address: {
-    street: String,
-    city: String,
-    state: String,
-    zipCode: String,
-    country: String
-  },
   isActive: {
     type: Boolean,
     default: true
   },
-  emailVerified: {
-    type: Boolean,
-    default: false
-  },
-  emailVerificationToken: String,
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
-  lastLogin: {
-    type: Date,
-    default: Date.now
-  },
-  preferences: {
-    newsletter: {
-      type: Boolean,
-      default: true
-    },
-    notifications: {
-      email: {
-        type: Boolean,
-        default: true
-      },
-      sms: {
-        type: Boolean,
-        default: false
-      }
-    },
-    theme: {
-      type: String,
-      enum: ['light', 'dark', 'auto'],
-      default: 'light'
-    }
-  },
-  socialProfiles: {
-    github: String,
-    linkedin: String,
-    twitter: String,
-    website: String
-  },
-  stats: {
-    totalOrders: {
-      type: Number,
-      default: 0
-    },
-    totalSpent: {
-      type: Number,
-      default: 0
-    },
-    favoriteProducts: [{
+  purchases: [{
+    product: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Product'
-    }]
+    },
+    purchaseDate: {
+      type: Date,
+      default: Date.now
+    },
+    amount: Number,
+    transactionId: String
+  }],
+  profile: {
+    phone: String,
+    country: String,
+    avatar: String
   }
 }, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Remove sensitive data from JSON output
+userSchema.methods.toJSON = function() {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
+
+module.exports = mongoose.model('User', userSchema);}, {
   timestamps: true,
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
